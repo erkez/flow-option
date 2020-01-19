43df1af76e2f7bcf9c7c3b8aaa8a28a263611e24
Alan Heitkotter <heitkotter@gmail.com>
2020-01-19 22:04:59 +0100

    Adds typescript definitions

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+declare module '@ekz/option' {
+
+    export class Option<A> {
+        private constructor();
+        get(): A;
+        readonly isEmpty: boolean
+        isDefined(): boolean;
+        map<B>(f: (value: A) => B): Option<B>;
+        mapNullable<B>(f: (value: A) => B | null | void): Option<B>;
+        flatMap<B>(f: (value: A) => Option<B>): Option<B>;
+        forEach(f: (value: A) => any): void;
+        filter(predicate: (value: A) => boolean): Option<A>;
+        getOrElse<B>(other: () => B): A | B;
+        getOrReturn<B>(other: B): A | B;
+        getOrUndefined(): A | void;
+        equals<B>(other: Option<B>): boolean;
+        toJSON(): unknown;
+
+        static of<V>(value?: V | null | void): Option<V>;
+
+        static None: Option<never>;
+        static Some<A>(value: A): Option<A>;
+    }
+
+    export const None: Option<never>;
+
+    export function Some<A>(value: A): Option<A>;
+
+}
